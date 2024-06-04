a2a746c62fce5ef01a482a58380e8fbcfd802c45
Sagar Tandan <076bct072@ioepc.edu.np>
2024-06-04 11:48:00 +0545

    removed app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home.jsx";
 import MovieDetail from "./Pages/MovieDetail.jsx";
@@ -14,19 +12,19 @@ import Flick from "./Pages/Flick.jsx";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:title" element={<MovieDetail />} />
-          <Route path="/:title/:ids" element={<MovieStream />} />
-          <Route path="/tv/:title/:ids" element={<TvStream />} />
-          <Route path="/tv/:title" element={<TvDetail />} />
-          <Route path="/flick" element={<Flick />} />
-          <Route path="/search" element={<SearchFunc />} />
-          <Route path="/movies" element={<AllMovies />} />
-          <Route path="/shows" element={<AllShows />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:title" element={<MovieDetail />} />
+        <Route path="/:title/:ids" element={<MovieStream />} />
+        <Route path="/tv/:title/:ids" element={<TvStream />} />
+        <Route path="/tv/:title" element={<TvDetail />} />
+        <Route path="/flick" element={<Flick />} />
+        <Route path="/search" element={<SearchFunc />} />
+        <Route path="/movies" element={<AllMovies />} />
+        <Route path="/shows" element={<AllShows />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
